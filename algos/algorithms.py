e071033bf6c60d3cdc00a76eec552bf3c76ea356
wvanamstel <wlvanamstel@gmail.com>
2015-01-13 18:27:19 -0800

    bin search

--- algos/algorithms.py
+++ algos/algorithms.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 class Algorithms(object):
 
     def __init__(self):
@@ -12,8 +11,29 @@ class Algorithms(object):
         for _ in xrange(n):
             yield x
             x, y = y, x + y
+            
+    def bin_search(self, item, lst, low, high):
+#==============================================================================
+#       Binary search
+#         IN: int, item to be found
+#             list, list of items to be searched
+#         OUT: index of item or -1 if item not found
+#==============================================================================
+        if low > high:
+            print 'not found'
+            return -1
+        midpoint = (low + high)/2
+        if lst[midpoint] == item:
+            print 'found ', midpoint
+            return midpoint
+        if lst[midpoint] > item:
+            self.bin_search(item, lst, low, midpoint)
+        elif lst[midpoint] < item:
+            self.bin_search(item, lst, midpoint+1, high)            
+
 
 if __name__ == '__main__':
     alg = Algorithms()
-    for i in alg.fibo(10):
-        print i
+    item = 2
+    lst = [0, 1, 2, 3, 4]
+    print alg.bin_search(item, lst, 0, len(lst) -1)
